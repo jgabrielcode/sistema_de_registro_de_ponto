c0c83acc0a2261fa31c4a0de1afaf3d796712f11
j_gabrielcode <joao1306gabriel@gmail.com>
2026-06-05 20:36:55 -0300

    Criacão das funcionalidades do botão de registro

--- registro.js
+++ registro.js
@@ -1,12 +1,15 @@
-let count = 0
+
         const botaoBaterPonto = document.getElementById("btn-bater-ponto");
         const entrada = document.querySelector("#entrada");
         const intervalo = document.querySelector("#intervalo");
         const retorno = document.querySelector("#retorno");
         const saida = document.querySelector("#saida");
 
+        let count = 0;
+  
+
       if (botaoBaterPonto) {
-        
+
         
         botaoBaterPonto.addEventListener("click", () => {
           botaoBaterPonto.textContent = "Ponto registrado";
@@ -14,26 +17,36 @@ let count = 0
           botaoBaterPonto.classList.add("btn-success");
           botaoBaterPonto.disabled = true;
 
+          
+
+          const data = new Date()
+          const hora = String(data.getHours()).padStart(2, "0");
+          const minutos = String(data.getMinutes()).padStart(2, "0");
+          
+
           setTimeout(() => {
             botaoBaterPonto.textContent = "Bater ponto";
             botaoBaterPonto.classList.remove("btn-success");
             botaoBaterPonto.classList.add("btn-primary");
             botaoBaterPonto.disabled = false;
+
           }, 1000);
+
+          
+
           if(count == 0){
-            entrada.textContent = "07:33"
-            count++
+            entrada.textContent = `${hora}:${minutos}`;
+            count++;
         }else if(count == 1){
-            intervalo.textContent = "12:39"
-            count++
+            intervalo.textContent = `${hora}:${minutos}`
+            count++;
         }else if(count == 2){
-            retorno.textContent = "13:51"
-            count++
+            retorno.textContent = `${hora}:${minutos}`
+            count++;
         }else if(count ==3){
-            saida.textContent = "17:35"
+            saida.textContent = `${hora}:${minutos}`
         }
-        
-        });
+      });
 
         
       }
